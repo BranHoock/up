0bfcac9856f8f2c9bbbb90b2392f2398f6a60f1a
BranHoock <90318755+BranHoock@users.noreply.github.com>
2021-12-03 15:08:28 +0300

    Add files via upload

---
+++ xyz/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дан массив, содержащий 10 элементов. Вычислить произведение элементов, стоящих после первого отрицательного элемента. Вывести исходный массив и результат вычислений
+Random rnd = new Random();
+double [] mas = new double[10];
+double s = 1;
+bool isNotFound= false;
+for (int i = 0; i < 10; i++)
+{
+    mas[i] = rnd.Next(-10,100);
+    Console.WriteLine("{0},", mas[i]);
+    if (isNotFound){s = s * mas[i];}
+    isNotFound|= mas[i] < 0;
+}
+Console.WriteLine("s={0},", s);
---
+++ xyz2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать метод, который вычисляет значения x = a2 и y = √a.Напечатать таблицу значений от –10 до 10 с шагом 1
+Console.WriteLine("Hello, World!");
+int []mas = new int[20];
+for (int i = 0; i < 20; i++)
+
+{
+    mas[i] = i - 10;
+
+double x= Math.Pow(mas[i],2);
+Console.WriteLine("x={0},", x);
+double y= Math.Sqrt(mas[i]);
+Console.WriteLine("y={0},", y);
+
+}
