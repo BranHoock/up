f61db6f6182191f2b56fde11095b9da8b92ecec9
BranHoock <90318755+BranHoock@users.noreply.github.com>
2021-12-06 09:53:05 +0300

    Add files via upload

---
+++ xyz3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дан массив, содержащий 14 элементов. Вычислить сумму элементов, стоящих до первого отрицательного элемента. Вывести исходный массив и результат вычислений
+Random rnd = new Random();
+double [] mas = new double[14];
+double s = 0;
+bool isNotFound= true;
+for (int i = 0; i < 14; i++)
+{
+    mas[i] = rnd.Next(-10,100);
+    Console.WriteLine("{0},", mas[i]);
+    isNotFound&= mas[i] > 0;
+    if (isNotFound){s = s + mas[i];}
+    
+}
+Console.WriteLine("s={0},", s);
